973d91600821af0f827b7cac0e503c9565cb1396
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2023-01-13 11:53:06 +0100

    Add the search spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -26,6 +26,11 @@ export default function Root() {
   const { contacts, q } = useLoaderData()
   const navigation = useNavigation()
   const submit = useSubmit()
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
   return (
     <>
       <div id="sidebar">
@@ -34,6 +39,7 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -43,7 +49,7 @@ export default function Root() {
                 submit(event.currentTarget.form)
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching}/>
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
